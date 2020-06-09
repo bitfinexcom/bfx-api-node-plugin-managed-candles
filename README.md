023cdb8913128254c151f50b80bc447534599fd2
Cris Mihalache <me@f3rno.com>
2020-06-09 14:29:40 +0700

    (refactor) finalize PR, bump to 1.0.3

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 [![Build Status](https://travis-ci.org/bitfinexcom/bfx-api-node-plugin-managed-candles.svg?branch=master)](https://travis-ci.org/bitfinexcom/bfx-api-node-plugin-managed-candles)
 
-This plugin maintains a full sorted candle set on its internal state, and emits it with every candle update with the event name `data:managed:candles`. The internal dataset is populated when receiving data from candle channels.
+This plugin maintains a full sorted candle set on its internal state, and emits
+it with every candle update with the event name `data:managed:candles`. The
+internal dataset is populated when receiving data from candle channels.
 
-Note that the manager proxies the event as `managed:candles`. If subscribing on a socket instance (`wsState.ev.on(...)`) use the internal event name, otherwise use the manager name with `manager.onWS(...)`.
+Note that the manager proxies the event as `managed:candles`. If subscribing on
+a socket instance (`wsState.ev.on(...)`) use the internal event name, otherwise
+use the manager name with `manager.onWS(...)`.
 
 ### Features
 
@@ -36,9 +40,11 @@ mgr.onWS('managed:candles', {}, (candles) => {
 
 ### Docs
 
-For an executable example, [see `examples/usage.js`](/examples/usage.js)
+API documentation can be found in [`docs/reference.md`](docs/reference.md), and
+examples in the [`examples`](examples) folder.
 
 ### Example
+
 ```js
 const debug = require('debug')('bfx:api:plugins:managed-candles:example')
 const { Manager, subscribe } = require('bfx-api-node-core')
--- index.js
+++ index.js
@@ -2,46 +2,47 @@
 
 /**
  * This module is a plugin for
- * {@link module:bfx-api-node-core|bfx-api-node-core} that maintains a full
- * sorted candle set on its internal state, and emits it with every candle
- * update with the event name `data:managed:candles`. The internal dataset is
- * populated when receiving data from candle channels.
+ * {@link external:bfx-api-node-core} that maintains a full sorted candle set
+ * on its internal state, and emits it with every candle update with the event
+ * name `data:managed:candles`. The internal dataset is populated when
+ * receiving data from candle channels.
  *
- * Note that the {@link module:bfx-api-node-core.Manager|Manager} proxies the
- * event as `managed:candles`. If subscribing on a socket instance
- * (`wsState.ev.on(...)`) use the internal event name, otherwise use the
- * manager name with `manager.onWS(...)`.
- *
- * ### Features
- *
- * * Maintains an up-to-date array of all received candles
- * * Emits a new 'managed:candles' event with the full dataset on each update
- *
- * ### Installation
- *
- * ```bash
- * npm i --save bfx-api-node-plugin-managed-candles
- * ```
- *
- * ### Quickstart
- *
- * ```js
- * const { Manager } = require('bfx-api-node-core')
- * const ManagedCandlesPlugin = require('bfx-api-node-plugin-managed-candles')
- *
- * const mgr = new Manager({
- *   plugins: [ManagedCandlesPlugin()]
- * })
- *
- * // set up a connection, event listeners, etc
- *
- * mgr.onWS('managed:candles', {}, (candles) => {
- *   debug(JSON.stringify(candles.toJS(), null, 2))
- * })
- * ```
+ * Note that the `Manager` proxies the event as `managed:candles`. If
+ * subscribing on a socket instance (`wsState.ev.on(...)`) use the internal
+ * event name, otherwise use the manager name with `manager.onWS(...)`.
  *
  * @license MIT
  * @module bfx-api-node-plugin-managed-candles
+ * @function
+ * @returns {bfx-api-node-core.Plugin} pluginState
+ * @example
+ * const debug = require('debug')('bfx:api:plugins:managed-candles:example')
+ * const { Manager, subscribe } = require('bfx-api-node-core')
+ * const ManagedCandlesPlugin = require('bfx-api-node-plugin-managed-candles')
+ *
+ * const SYMBOL = 'tBTCUSD'
+ * const mgr = new Manager({
+ *   transform: true,
+ *   plugins: [ManagedCandlesPlugin()]
+ * })
+ *
+ * mgr.onWS('open', {}, () => debug('connection open'))
+ *
+ * // Receive updated dataset
+ * mgr.onWS('managed:candles', {}, (candles) => {
+ *   debug('updated dataset for %s contains %d candles', SYMBOL, candles.length)
+ *   debug('latest candle follows')
+ *   debug(JSON.stringify(candles[0].toJS(), null, 2))
+ * })
+ *
+ * const wsState = mgr.openWS()
+ *
+ * subscribe(wsState, 'candles', { key: `trade:1m:${SYMBOL}` })
+ */
+
+/**
+ * @external bfx-api-node-core
+ * @see https://github.com/bitfinexcom/bfx-api-node-core
  */
 
 module.exports = require('./lib/plugin')
